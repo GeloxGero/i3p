1771ee63b42d06d1d0136f730ea4cd783109085f
GeloxGero <gelo12gero@gmail.com>
2026-03-15 12:16:40 +0800

    config fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import { node } from "@astrojs/node"; // Add this import
 import tailwindcss from "@tailwindcss/vite";
 import { heroui } from "@heroui/react";
-
-import react from "@astrojs/react";
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
 	output: "server",
+	adapter: node(), // Add this line
 	integrations: [react()],
 });
