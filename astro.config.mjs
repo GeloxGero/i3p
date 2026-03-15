6def6406e5fa661098a9e4873687edeb75abf8a8
GeloxGero <gelo12gero@gmail.com>
2026-03-15 12:44:03 +0800

    render hosting prerequisuites

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import { node } from "@astrojs/node"; // Add this import
 import tailwindcss from "@tailwindcss/vite";
 import { heroui } from "@heroui/react";
+import node from "@astrojs/node";
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	adapter: node({
+		mode: "standalone",
+	}),
 	output: "server",
-	adapter: node(), // Add this line
 	integrations: [react()],
 });
